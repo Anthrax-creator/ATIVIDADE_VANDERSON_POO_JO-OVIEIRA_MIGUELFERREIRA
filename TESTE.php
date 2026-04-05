411169c91e9acdf5ffb1b087d7eda89ec9a00907
Anthrax-creator <jvieirademartin@gmail.com>
2026-04-05 17:03:25 -0300

    inserção de teste

---
+++ TESTE.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "paciente.php";
+
+$paciente = new Paciente("João", "Masculino", "2005-10-10", "Asma");
+
+echo "Nome: " . $paciente->getNome() . "<br>";
+echo "Sexo: " . $paciente->getSexo() . "<br>";
+echo "Nascimento: " . $paciente->getNascimento() . "<br>";
+echo "Enfermidades: " . $paciente->getEnfermidadesPreexistentes() . "<br>";
+
+echo "". "<br>";
+
+require_once "medico.php";
+
+$medico = new Medico("Estevão", "Masculino", "2000-09-07", "12345", "Otorrino");
+
+echo "Nome: " . $paciente->getNome() . "<br>";
+echo "Sexo: " . $paciente->getSexo() . "<br>";
+echo "Nascimento: " . $paciente->getNascimento() . "<br>";
+echo "Crm: " . $medico->getCrm() . "<br>";
+echo "Especialidade: " . $medico->getEspecialidade() . "<br>";
+
+?>
