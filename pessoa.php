3def6c76020003a60c4e1d43408d3085270e26fd
Anthrax-creator <jvieirademartin@gmail.com>
2026-04-05 11:23:02 -0300

    Primeira classe Pessoa

---
+++ pessoa.php
@@ -0,0 +1,40 @@
+<?php
+
+class Pessoa {
+    protected $nome;
+    protected $sexo;
+    protected $nascimento;
+
+    public function __construct($nome, $sexo, $nascimento) {
+        $this->nome = $nome;
+        $this->sexo = $sexo;
+        $this->nascimento = $nascimento;    
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getSexo() {
+        return $this->sexo;
+    }
+
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    public function getNascimento() {
+        return $this->nascimento;
+    }
+
+    public function setNascimento($nascimento) {
+        $this->nascimento = $nascimento
+    }
+
+}
+
+?>
